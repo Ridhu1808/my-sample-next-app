0a20438530bbdf1dbede602167e3f34c7739c759
Ridhu1808 <ridhu.varshini@changecx.com>
2024-08-06 14:16:24 +0530

    Req body

---
+++ src/app/api/user/route.js
@@ -0,0 +1,22 @@
+import { getRequestContext } from "@cloudflare/next-on-pages";
+
+export const runtime = "edge";
+
+export async function GET(request, Response) {
+  let responseText = "Hello World------helo";
+  let username = req.body();
+  // In the edge runtime you can use Bindings that are available in your application
+  // (for more details see:
+  //    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
+  //    - https://developers.cloudflare.com/pages/functions/bindings/
+  // )
+  //
+  // KV Example:
+  // const myKv = getRequestContext().env.MY_KV_NAMESPACE
+  // await myKv.put('suffix', ' from a KV store!')
+  // const suffix = await myKv.get('suffix')
+  // responseText += suffix
+
+  // const myKv = getRequestContext().env.MY_KV_NAMESPACE;
+  return new Response(responseText, username);
+}
